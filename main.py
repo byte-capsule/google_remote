e07a96d8ff807699d5a2e48c9e887bf58b39d0e9
Jeshan Akand <79209625+byte-capsule@users.noreply.github.com>
2023-10-29 11:43:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,41 @@
-import requests,base64,random,secrets,json,operator
-from requests.structures import CaseInsensitiveDict
+import cv2
 
+def add_logo_to_video(video_path, logo_path, output_path, x=50, y=50):
+    """
+    Adds a logo image to each frame of a video.
 
+    Args:
+        video_path (str): Path to the input video file.
+        logo_path (str): Path to the logo image file.
+        output_path (str): Path to save the output video.
+        x (int): X-coordinate for logo placement (default: 50).
+        y (int): Y-coordinate for logo placement (default: 50).
+    """
+    cap = cv2.VideoCapture(video_path)
+    img = cv2.imread(logo_path)
 
+    # Get logo dimensions
+    img_height, img_width, _ = img.shape
 
-class Base64Jx:
-	def __init__(self,text,amount):
-		self.text=text
-		self.amount=amount
-		
-		
-	def b64en(self,type):
-		for I in range(self.amount):
-			self.text=base64.b64encode(self.text.encode(type)).decode(type)
-		return self.text
-	def b64de(self,type):
-		for j in range(self.amount):
-			self.text=base64.b64decode(self.text.encode(type)).decode(type)
-		return self.text
+    # Get frame dimensions
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_path, fourcc, 30.0, (frame_width, frame_height))
 
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
+        # Add the logo to the frame
+        frame[y:y+img_height, x:x+img_width] = img
 
-def save_data(data):
-	import json
-	with open("save_data.json","w")as F:
-		json.dump(data,F,sort_keys=True,indent=2)
+        out.write(frame)
 
-def load_data(data):
-	import json
-	with open("save_data.json","r") as R:
-		all_data=json.load(R)
-		
+    cap.release()
+    out.release()
 
-
-
-
-def cheak_balance(uid,token):
-	data='{"sal":"'+secrets.token_hex(15)+'","sig":"'+str(random.randrange(100,999))+'","mid":"'+str(uid)+'","tid":"'+str(random.randrange(1000,9999))+'","data":"","pack":"com.app.cozyrewardsnewtop"}'
-	data=Base64Jx(data,2).b64en("ascii")
-	url = "https://netravpn.payzonebd.xyz/api/v1/user_coin?data="+data
-	#ZXlKellXd2lPaUkzT0RobFlqSXhNelZrTlRjME5XRXdOR1JqTTJRMU16QXhZek13TVRrek1pSXNJ%0Abk5wWnlJNklqRTJPU0lzSW0xcApaQ0k2SWpFek9UQXhJaXdpZEdsa0lqb2lOVGc1TkNJc0ltUmhk%0AR0VpT2lJaUxDSndZV05ySWpvaVkyOXRMbUZ3Y0M1amIzcDVjbVYzCllYSmtjMjVsZDNSdmNDSjkK%0A"
-
-	headers = CaseInsensitiveDict()
-	headers["Host"] = "dme.payzonebd.xyz"
-	headers["accept"] = "application/json"
-	headers["authorization"] =token #"Bearer 290600|DZjc6TJ3tTFkLhLCyNpB7yxU41t5yg4eqDrDZhZY"
-	headers["user-agent"] = "AAAAAAAAcgwcEQkB"
-	headers["accept-encoding"] = "gzip"
-	
-	
-	resp = requests.get(url, headers=headers,verify=True)
-	#print(resp.text)
-	try:
-		json_data=json.loads(resp.text)
-		
-		baleance=json_data["data"]
-		
-		
-		if baleance !="0" and int(baleance) > 15000:
-			print(baleance,"   ",uid)
-			local_data={
-			"Balance":int(baleance),
-			"U_id":uid,
-			
-			}
-			all_data.append(local_data)
-			x=int(baleance)/150
-			return str(x)+"  TK"
-		
-	except:
-				return ''
-				None
-				
-				#print(resp.text)
-				
-				
-	
-	#json_raw_data =json.loads(resp.text)
-token=input("Enter token : ")	
-all_data=[]
-for i in range(1,10000):
-	x=cheak_balance(str(i),token)
-	print("-------------------------------------------------------------")
-	print(i,"-----",x)
-	all_data = sorted(all_data, key=operator.itemgetter('Balance'),reverse=True)
-	save_data(all_data)
-	
-	
+# Example usage:
+add_logo_to_video('input_video.mp4', 'logo.png', 'output_video.mp4')
